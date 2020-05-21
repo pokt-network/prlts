3ad781b727d57b1adb1f0efebd8cabaee7c9155f
Luis de Leon <luyzdeleon@gmail.com>
2020-05-21 10:01:12 -0400

    Tweaked relay time measurement to leave out dispatch loading times

--- src/relayer/dispatch.js
+++ src/relayer/dispatch.js
@@ -25,21 +25,34 @@ class DispatchRelayer {
             const aat = getRandomFromArray(aats[chain.hash])
             // Now pick a random payload from the chain
             const payload = getRandomFromArray(chain.payloads)
-            // Measure relay execution time
-            const startTime = new Date();
             // Send the relay
             let responseOrError = undefined
             // Set the configuration for pocket
-            const session = await pocket.sessionManager.getCurrentSession(aat, payload.blockchain, pocket.configuration)
+            const session = await pocket.sessionManager.getCurrentSession(
+                aat,
+                payload.blockchain,
+                pocket.configuration
+            )
             if (typeGuard(session, RpcError)) {
                 throw session
             }
 
+            // Measure relay execution time
+            const startTime = new Date()
             // Set the node
             let node = undefined
             if (payload.consensus_enabled === true) {
                 //logger.debug(session)
-                responseOrError = await pocket.sendConsensusRelay(payload.data, payload.blockchain, aat, pocket.configuration, undefined, payload.method, payload.path, undefined)
+                responseOrError = await pocket.sendConsensusRelay(
+                    payload.data,
+                    payload.blockchain,
+                    aat,
+                    pocket.configuration,
+                    undefined,
+                    payload.method,
+                    payload.path,
+                    undefined
+                )
             } else {
                 // Get the node to be able to log it before sending the relay
                 const nodeOrError = session.getSessionNode()
@@ -63,13 +76,13 @@ class DispatchRelayer {
             }
             // Measure relay execution time
             const endTime = new Date()
-            const timeDiff = endTime - startTime; //in ms
+            const timeDiff = endTime - startTime //in ms
             const analytics = {
                 success: false,
                 errorMsg: undefined,
                 relayTime: 0,
                 node: undefined,
-                session: session
+                session: session,
             }
             // Fill analytics
             analytics.relayTime = timeDiff
@@ -78,21 +91,28 @@ class DispatchRelayer {
             }
 
             // Parse the response or error and log it
-            if (responseOrError === undefined || typeGuard(responseOrError, RpcError) || typeGuard(responseOrError, Error)) {
-                analytics.errorMsg = responseOrError.message === undefined ? "Undefined response" : responseOrError.message
+            if (
+                responseOrError === undefined ||
+                typeGuard(responseOrError, RpcError) ||
+                typeGuard(responseOrError, Error)
+            ) {
+                analytics.errorMsg =
+                    responseOrError.message === undefined
+                        ? "Undefined response"
+                        : responseOrError.message
             } else {
                 analytics.success = true
             }
             return analytics
         } catch (error) {
             this.logger.log("error", "Error submitting relay", error)
-            if (typeof session !== 'undefined') {
+            if (typeof session !== "undefined") {
                 return {
                     success: false,
                     errorMsg: error.message,
                     relayTime: 0,
                     node: undefined,
-                    session: session
+                    session: session,
                 }
             } else {
                 return {
@@ -100,7 +120,7 @@ class DispatchRelayer {
                     errorMsg: error.message,
                     relayTime: 0,
                     node: undefined,
-                    session: undefined
+                    session: undefined,
                 }
             }
         }
@@ -109,15 +129,26 @@ class DispatchRelayer {
     async config() {
         // Parse the dispatchers list
         const dispatchURLList = []
-        for (let index = 0; index < this.configuration.dispatchers.length; index++) {
+        for (
+            let index = 0;
+            index < this.configuration.dispatchers.length;
+            index++
+        ) {
             dispatchURLList.push(new URL(this.configuration.dispatchers[index]))
         }
-        this.logger.log("debug", "Successfully parsed dispatch url list", dispatchURLList)
+        this.logger.log(
+            "debug",
+            "Successfully parsed dispatch url list",
+            dispatchURLList
+        )
 
         // Create a keybase and a client account that will sign all relays
         const keybase = new Pocket(dispatchURLList).keybase
         const clientAccount = await keybase.createAccount(this.clientPassphrase)
-        const clientPrivateKeyOrError = await keybase.exportAccount(clientAccount.addressHex, this.clientPassphrase)
+        const clientPrivateKeyOrError = await keybase.exportAccount(
+            clientAccount.addressHex,
+            this.clientPassphrase
+        )
         if (typeGuard(clientPrivateKeyOrError, Error)) {
             throw clientPrivateKeyOrError
         }
@@ -125,12 +156,19 @@ class DispatchRelayer {
         // Load application AAT's
         this.aats = {}
         for (let index = 0; index < this.configuration.chains.length; index++) {
-            const chainConfig = this.configuration.chains[index];
+            const chainConfig = this.configuration.chains[index]
             this.aats[chainConfig.hash] = []
-            for (let j = 0; j < chainConfig.application_private_keys.length; j++) {
-                const appPK = chainConfig.application_private_keys[j];
+            for (
+                let j = 0;
+                j < chainConfig.application_private_keys.length;
+                j++
+            ) {
+                const appPK = chainConfig.application_private_keys[j]
                 // Create an application account so we can extract the public key
-                const appAccount = await keybase.importAccount(Buffer.from(appPK, "hex"), this.applicationPassphrase)
+                const appAccount = await keybase.importAccount(
+                    Buffer.from(appPK, "hex"),
+                    this.applicationPassphrase
+                )
                 const pocketAAT = await PocketAAT.from(
                     "0.0.1",
                     clientAccount.publicKey.toString("hex"),
@@ -143,14 +181,28 @@ class DispatchRelayer {
         // Log aats
         this.logger.log("debug", "Successfully configurated AATs", this.aats)
 
-        for (let index = 0; index < this.configuration.parallelRelays; index++) {
+        for (
+            let index = 0;
+            index < this.configuration.parallelRelays;
+            index++
+        ) {
             const localPocketInstance = new Pocket(dispatchURLList)
-            const account = await localPocketInstance.keybase.importAccount(clientPrivateKey, this.clientPassphrase)
-            await localPocketInstance.keybase.unlockAccount(account.addressHex, this.clientPassphrase, 0)
+            const account = await localPocketInstance.keybase.importAccount(
+                clientPrivateKey,
+                this.clientPassphrase
+            )
+            await localPocketInstance.keybase.unlockAccount(
+                account.addressHex,
+                this.clientPassphrase,
+                0
+            )
             this.pocketInstances.push(localPocketInstance)
         }
         // Log pocket instances
-        this.logger.log("debug", `Succesfully configured ${this.configuration.parallelRelays} pocket instances`)
+        this.logger.log(
+            "debug",
+            `Succesfully configured ${this.configuration.parallelRelays} pocket instances`
+        )
     }
 
     // Start the relayer
@@ -160,8 +212,21 @@ class DispatchRelayer {
 
         while (true) {
             const tasks = []
-            for (let index = 0; index < this.configuration.parallelRelays; index++) {
-                const pocketConfiguration = new PocketConfiguration(undefined, undefined, 5, this.configuration.relayTimeout, true, this.configuration.sessionBlockFrequency, this.configuration.blockTime, undefined)
+            for (
+                let index = 0;
+                index < this.configuration.parallelRelays;
+                index++
+            ) {
+                const pocketConfiguration = new PocketConfiguration(
+                    undefined,
+                    undefined,
+                    5,
+                    this.configuration.relayTimeout,
+                    true,
+                    this.configuration.sessionBlockFrequency,
+                    this.configuration.blockTime,
+                    undefined
+                )
                 const pocket = this.pocketInstances[index]
                 pocket.configuration = pocketConfiguration
                 tasks.push(this.submitRandomRelay(this.aats, pocket))
